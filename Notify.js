b4b86061f6d633e31a7a74d029ac144f0df9021a
Yannis Kolovos <yannis.kolovos@gmail.com>
2015-04-23 10:26:13 +0200

    adding padding

--- Notify.js
+++ Notify.js
@@ -11,6 +11,7 @@ Notify = function(text, callback, close_callback, style) {
   $('<a>',{
 	  text: '×',
 	  class: 'button close',
+	  style: 'padding-left: 10px;',
 	  href: '#',
 	  click: function(e){
 		  e.preventDefault()

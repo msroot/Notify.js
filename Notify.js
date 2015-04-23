d2935becd0025e6ef6edc09e325080f00ab5186b
Ioannis Kolovos <yannis.kolovos@gmail.com>
2015-04-23 13:20:09 +0200

    adding mouse over events

--- Notify.js
+++ Notify.js
@@ -1,43 +1,52 @@
 Notify = function(text, callback, close_callback, style) {
 
-  var time = '10000';
-  var $container = $('#notifications');
-  var icon = '<i class="fa fa-info-circle "></i>';
+	var time = '10000';
+	var $container = $('#notifications');
+	var icon = '<i class="fa fa-info-circle "></i>';
  
-  if (typeof style == "undefined" ) style = 'warning'
+	if (typeof style == 'undefined' ) style = 'warning'
   
-  var html = $('<div class="alert alert-' + style + '  hide">' + icon +  " " + text + '</div>');
+		var html = $('<div class="alert alert-' + style + '  hide">' + icon +  " " + text + '</div>');
   
-  $('<a>',{
-	  text: '×',
-	  class: 'button close',
-	  style: 'padding-left: 10px;',
-	  href: '#',
-	  click: function(e){
-		  e.preventDefault()
-		  close_callback && close_callback()
-		  NotifyX(html)
-	  }
-  }).prependTo(html)
+	$('<a>',{
+		text: '×',
+		class: 'button close',
+		style: 'padding-left: 10px;',
+		href: '#',
+		click: function(e){
+			e.preventDefault()
+			close_callback && close_callback()
+			NotifyX(html)
+		}
+	}).prependTo(html)
 
-  $container.prepend(html)
-  html.removeClass('hide').hide().fadeIn('slow')
+	$container.prepend(html)
+	html.removeClass('hide').hide().fadeIn('slow')
   
-  html.on('click', function () {
-	callback && callback()
-  	NotifyX(html)
-  });
+	html.on('click', function () {
+		callback && callback()
+		NotifyX(html)
+	});
 
-  setTimeout(function () {
-    NotifyX(html);
-  }, time);
+	function remove_notice() {
+		NotifyX(html)
+	}
+	var timer =  setInterval(remove_notice, time);
+
+	$(html).hover(function(){
+		clearInterval(timer);
+	}, function(){
+		timer = setInterval(remove_notice, time);
+	});
+  
+  
 }
 
 NotifyX = function(element) {
-  if (typeof element !== "undefined" ) {
-	element.stop().fadeOut("slow").remove();
-  } else {
-	  var last = $('#notifications').children('.alert').last()
-	  last.stop().fadeOut("slow").remove();
-  }
+	if (typeof element !== "undefined" ) {
+		element.stop().fadeOut("slow").remove();
+	} else {
+		var last = $('#notifications').children('.alert').last()
+		last.stop().fadeOut("slow").remove();
+	}
 }
